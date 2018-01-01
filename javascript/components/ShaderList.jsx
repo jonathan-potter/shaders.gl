358c4af29e01becf3a51a515a75bcc10662f6903
jonathan potter <jonathan.potter.business@gmail.com>
2017-12-31 23:48:12 -0800

    mousing in and out of shaders on index page will start and stop animation

--- javascript/components/ShaderList.jsx
+++ javascript/components/ShaderList.jsx
@@ -1,7 +1,6 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react'
 import { connect } from 'react-redux'
-import initializeWebGL from 'webgl/initializeWebGL'
+import ShaderListItem from 'components/ShaderListItem'
 import * as actions from 'actions'
 
 import './ShaderList.scss'
@@ -9,58 +8,11 @@ import './ShaderList.scss'
 const mapStateToProps = ({ shaders }) => ({ shaders })
 
 export default connect(mapStateToProps, actions)(
-  ({ setCurrentShader, shaderId, shaders, store }) => {
+  ({ shaders, store }) => {
     return (
       <ul className='container shader-list'>
-        {Object.values(shaders).map(shader => <ListItem key={shader.id} shader={shader} store={store} />)}
+        {Object.values(shaders).map(shader => <ShaderListItem key={shader.id} shader={shader} store={store} />)}
       </ul>
     )
   }
 )
-
-class ListItem extends Component {
-  componentDidMount () {
-    const { shader, store } = this.props
-    const { canvas } = this.refs
-
-    initializeWebGL({
-      canvas,
-      shaderId: shader.id,
-      singleFrame: true,
-      store
-    })
-  }
-
-  render () {
-    const { shader } = this.props
-
-    return (
-      <Link onMouseOver={this.onMouseOver} onMouseOut={this.onMouseOut} className='shader-card' to={`/shader/${shader.id}`}>
-        <canvas width='300' height='200' ref='canvas' className='shader-card__canvas' />
-        <div className='shader-card__hero'>
-          <span className='shader-card__title'>{shader.name}</span>
-        </div>
-      </Link>
-    )
-  }
-
-  onMouseOver = () => {
-    console.log('mouseover')
-    const { shader, store } = this.props
-    const { canvas } = this.refs
-
-    initializeWebGL({
-      canvas,
-      shaderId: shader.id,
-      singleFrame: true,
-      store
-    })
-  }
-
-  onMouseOut = () => {
-    console.log('mouseout')
-    this.props.resetRenderContext()
-  }
-}
-
-connect(() => {}, actions)(ListItem)
